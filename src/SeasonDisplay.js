0ea426d24e40becde30e64bd1cc4f324898023ec
HarshaPrajapati <harsha_1605@yahoo.com>
2020-12-27 12:36:59 +0530

    seasons project and clock

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const seasonConfig = {
+    winter : {
+    text: "It is chilly",
+    iconName: 'snowflake'
+    },
+    summer : {
+    text: 'Lets hit the beach',
+    iconName: 'sun'
+    }
+}
+const getSeason = (lat, month) => {
+    if(month > 2 && month < 9){
+      return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+    const SeasonDisplay = (props) =>{
+        const season = getSeason(props.lat , new Date().getMonth());
+        const {text , iconName} = seasonConfig[season];
+        return(
+        <div className={`season-display ${season}`}>
+            <i className={`${iconName} icon massive icon-left`} />
+            <h2 className="header">{text}</h2>
+            <i className={`${iconName} icon massive icon-right`} />
+        </div>
+    )
+}
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './App.css';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+import Clock from './Clock';
 
+//app class is borrowing fnt's & methods from base class react.component
+class App extends React.Component {
+  constructor(props){
+      //very frst fn to get called whenever the instance of classs is created
+      super(props);
+       //super is a refrence to the parents constructor fn
+       this.state = {
+           latitude: null,
+           errorMessage:''
+       };
+       
+  }
+  componentDidMount(){
+    window.navigator.geolocation.getCurrentPosition(
+        position => this.setState({latitude: position.coords.latitude}), 
+        (err) => this.setState({errorMessage: err.message}));
+  }
+    renderContent(){
+        if(this.state.errorMessage && !this.state.latitude){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        if(!this.state.errorMessage && this.state.latitude){
+          return <SeasonDisplay lat={this.state.latitude} />
+        }
+        return (
+        <Spinner message="Please accept the request"/>
+        )
+    }
+  //react says we have to define rednder
+    render(){
+       return(
+        <div className="border red">
+            {this.renderContent()}
+            <Clock />
+        </div>
+       )
+    }
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector('#root')
+)
